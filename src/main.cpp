87ff517dd7a8d18a47b126c4a667138c834e37e1
Luca Mazza <luca.mazza@mazluc.ch>
2026-07-01 14:55:01 +0200

    feat: base+

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#define XR_USE_GRAPHICS_API_VULKAN
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
+
+#include <webgpu/webgpu_cpp.h>
+#include <dawn/native/DawnNative.h>
+#include <dawn/native/VulkanBackend.h>
+
+int main() {
+
+}
