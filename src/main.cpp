d883210fad18fbc50f56252fbc1d02085f5b761f
Luca Mazza <luca.mazza@mazluc.ch>
2026-07-02 11:39:44 +0200

    feat(dawn): base initializations working

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
-#define XR_USE_GRAPHICS_API_VULKAN
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <vulkan/vulkan.h>
 
 #include <iostream>
 #include <vector>
@@ -12,5 +16,36 @@
 #include <dawn/native/VulkanBackend.h>
 
 int main() {
+	std::cout << "Starting main demo..." << std::endl;
 
+	// OpenXR Instance Initialization
+	const char* requestedExtensions[] = {
+		XR_KHR_VULKAN_ENABLE_EXTENSION_NAME
+	};
+	XrInstanceCreateInfo xrInstanceCreateInfo{XR_TYPE_INSTANCE_CREATE_INFO};
+	strcpy_s(xrInstanceCreateInfo.applicationInfo.applicationName, "Main_Demo");
+	xrInstanceCreateInfo.applicationInfo.apiVersion = XR_API_VERSION_1_0;
+
+	xrInstanceCreateInfo.enabledExtensionCount = sizeof(requestedExtensions) / sizeof(requestedExtensions[0]);
+	xrInstanceCreateInfo.enabledExtensionNames = requestedExtensions;
+
+	XrInstance xrInstance;
+	XrResult res = xrCreateInstance(&xrInstanceCreateInfo, &xrInstance);
+	if (XR_FAILED(res)) {
+		std::cerr << "[OpenXR]: Failed to create OpenXR instance: " << res << std::endl;
+		return -1;
+	}
+	std::cout << "[OpenXR]: OpenXR instance created successfully." << std::endl;
+
+	// TODO: Ask OpenXR for Vulkan requirements and init Dawn
+	
+	// Main loop
+	bool isRunning = true;
+	while (isRunning) {
+		// TODO: Render Loop
+	}
+
+	// Cleanup
+	xrDestroyInstance(xrInstance);
+	return 0;
 }
