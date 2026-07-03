09beda67bf518a86477a1cab8d386bbef6636185
Luca Mazza <luca.mazza@mazluc.ch>
2026-07-03 11:50:43 +0200

    feat: non working solution

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,12 @@
 #include <webgpu/webgpu_cpp.h>
 #include <dawn/native/DawnNative.h>
 #include <dawn/native/VulkanBackend.h>
+#include "dawn/native/vulkan/DeviceVk.h"
+
+#define XR_CHECK(res) if (XR_FAILED(res)) { \
+    std::cerr << "OpenXR Error Code: " << res << " at line " << __LINE__ << std::endl; \
+    exit(-1); \
+}
 
 int main() {
 	std::cout << "Starting main demo..." << std::endl;
@@ -37,7 +43,54 @@ int main() {
 	}
 	std::cout << "[OpenXR]: OpenXR instance created successfully." << std::endl;
 
+	XrSystemGetInfo systemInfo{XR_TYPE_SYSTEM_GET_INFO};
+	systemInfo.formFactor = XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY;
+	XrSystemId xrSystemId;
+	XR_CHECK(xrGetSystem(xrInstance, &systemInfo, &xrSystemId));
+
 	// TODO: Ask OpenXR for Vulkan requirements and init Dawn
+	
+	std::cout << "[Dawn]: Initializing Dawn with Vulkan backend..." << std::endl;
+
+	XrGraphicsRequirementsVulkanKHR vkReqs{XR_TYPE_GRAPHICS_REQUIREMENTS_VULKAN_KHR};
+	PFN_xrGetVulkanGraphicsRequirementsKHR pfnGetVulkanGraphicsRequirementsKHR = nullptr;
+	xrGetInstanceProcAddr(xrInstance, "xrGetVulkanGraphicsRequirementsKHR", (PFN_xrVoidFunction*)&pfnGetVulkanGraphicsRequirementsKHR);
+	XR_CHECK(pfnGetVulkanGraphicsRequirementsKHR(xrInstance, xrSystemId, &vkReqs));
+
+	wgpu::InstanceDescriptor instanceDesc{};
+	auto dawnInstance = std::make_unique<dawn::native::Instance>(&instanceDesc);
+	dawn::native::Adapter vkAdapter;
+	bool adapterFound = false;
+
+	std::vector<dawn::native::Adapter> nativeAdapters = dawnInstance->EnumerateAdapters();
+	for (auto& nativeAdapter : nativeAdapters) {
+		wgpu::Adapter adapter = wgpu::Adapter(nativeAdapter.Get());
+		wgpu::AdapterInfo info{};
+		adapter.GetInfo(&info);
+		if (info.backendType == wgpu::BackendType::Vulkan) {
+			vkAdapter = nativeAdapter;
+			adapterFound = true;
+			std::cout << "[Dawn]: Vulkan adapter found" << std::endl;
+			break;
+		}
+	}
+	if (!adapterFound) {
+		std::cerr << "[Dawn]: No Vulkan adapter found" << std::endl;
+		return -1;
+	}
+
+	wgpu::DeviceDescriptor deviceDesc{};
+	wgpu::Device device = vkAdapter.CreateDevice(&deviceDesc);
+
+	WGPUDevice cDevice = device.Get();
+	auto* internalVkDevice = reinterpret_cast<dawn::native::vulkan::Device*>(cDevice);
+	VkDevice vkDevice = internalVkDevice->GetVkDevice();
+
+	if (vkDevice == VK_NULL_HANDLE) {
+		std::cerr << "[Dawn]: Failed to retrieve Vulkan device from Dawn" << std::endl;
+		return -1;
+	}
+	std::cout << "[Dawn]: Vulkan device retrieved successfully from Dawn" << std::endl;
 	
 	// Main loop
 	bool isRunning = true;
