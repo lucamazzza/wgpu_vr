049c6cc81e92929b088564d924981e89f08eff34
Luca Mazza <luca.mazza@mazluc.ch>
2026-07-10 15:32:10 +0200

    feat: main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
 #include "XrWGPUBridge.h"
 
+#include <iostream>
+#include <exception>
+
 int main() {
+    try {
+        std::cout << "Starting VR Application" << std::endl;
+        XrWGPUBridge vrBridge;
+        vrBridge.Initialize();
+        bool isRunning = true;
+        while(isRunning) {
+            // TODO: Render Loop
+            isRunning = vrBridge.RenderFrame();
+        }
+        std::cout << "Shutting down gracefully" << std::endl;
+    } catch (const std::exception &e) {
+        std::cerr << "Fatal error: " << e.what() << std::endl;
+        return -1;
+    }
+    return 0;
 }
